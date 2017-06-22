7a4da7d3ceb29a1d62b7a937a5cb03cbfb91953c
JangTaeGyu <ttggbbgg2@gmail.com>
2017-06-22 17:26:40 +0900

    navbar 추가

---
+++ template/layout/navbar.php
@@ -0,0 +1,11 @@
+
+<div class="navbar">
+    <div class="navbar_brand">
+        <a href="/">메모장 웹 어플리케이션</a>
+    </div>
+    <ul class="navbar_list">
+        <li class="navbar_item"><a href="/auth/login">로그인</a></li>
+        <li class="navbar_item"><a href="/join/signup">가입하기</a></li>
+        <li class="navbar_item"><a href="/auth/logout">로그아웃</a></li>
+    </ul>
+</div>
--- template/layout/top.php
+++ template/layout/top.php
@@ -4,6 +4,11 @@
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=Edge">
         <meta http-equiv="imagetoolbar" content="no">
-        <title>메모장 :: <?= isset($title) ? $title : '' ?></title>
+        <title>메모장 웹 어플리케이션 :: <?= isset($title) ? $title : '' ?></title>
+
+        <link rel="stylesheet" href="/css/memo.css">
     </head>
     <body>
+        <div class="container">
+
+            <?php echo view('layout/navbar.php'); ?>
